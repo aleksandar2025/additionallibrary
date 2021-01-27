445bfc3de42839d6e5930d340c4db603b494a2ae
Samsara636 <aleksandar.ivanovicbl@gmail.com>
2021-01-27 08:29:41 +0100

    modified:   defer.php added newer wordpress support

--- repo/DoTation/defer.php
+++ repo/DoTation/defer.php
@@ -9,3 +9,19 @@ function defer_parsing_of_js($url)
   return str_replace(' src', ' defer src', $url);
 }
 add_filter('script_loader_tag', 'defer_parsing_of_js', 10);
+
+//defer 3rd party js for newer version of wordpress
+function defer_parsing_of_js($url)
+{
+  if (is_admin()) return $url; //don't break WP Admin
+  if (false === strpos($url, '.js')) return $url;
+  if (strpos($url, 'jquery.min.js')) return $url;
+
+  if (strpos($url, 'jquery-migrate.js')) return $url;
+  return str_replace(' src', ' defer src', $url);
+}
+add_filter('script_loader_tag', 'defer_parsing_of_js', 10);
+
+
+
+
